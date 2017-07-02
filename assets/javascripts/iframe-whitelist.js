ea466420fc2c5291bd12fa66195db44b07a72d87
yaat <yaat@users.noreply.github.com>
2017-07-02 18:09:08 +0200

    Update iframe-whitelist.js

--- assets/javascripts/iframe-whitelist.js
+++ assets/javascripts/iframe-whitelist.js
@@ -3,7 +3,6 @@
 
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.freesound\.org\/embed\/sound\/iframe\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/w\.soundcloud\.com\/player\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/graphics8\.nytimes\.com\/.+/i);
   Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/www\.youtube\.com\/embed\/.+/i);
-  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/discordapp\.com\/.+/i);
+  Discourse.Markdown.whiteListIframe(/^(https?:)?\/\/bjj\.li\/.+/i);  
 })();
